3d88483e73b4cb1e59b68dab9bb3b93a1262696f
Mai Xuan Bach <maixuanbach174@gmail.com>
2023-06-27 14:41:46 +0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -9,10 +9,10 @@ int main()
     m.insert("A fruit which is red", "apple");
     m.insert("a common animal with four legs, especially kept by people as a pet or to hunt or guard things", "dog");
     m.insert("an animal", "cat");
-    cout << m.insert("hihi", "cuoi") << endl;
-    // cout << m.erase("an animal") << endl;
-    // cout << m.find("an animal") << endl;
-    cout << m.find("a common animal with four legs, especially kept by people as a pet or to hunt or guard things")->val << endl;
-    cout << m.find("A fruit which is red")->val << endl;
+    cout << m.erase("an animal") << endl;
+    cout << m.erase("A fruit which is red") << endl;
+    cout << m.erase("a common animal with four legs, especially kept by people as a pet or to hunt or guard things") << endl;
+    cout << m.find("a common animal with four legs, especially kept by people as a pet or to hunt or guard things") << endl;
+    cout << m.find("A fruit which is red") << endl;
     return 0;
 }
