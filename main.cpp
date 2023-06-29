5d94079adb0ebf564b469e6e7774e99f88e80d96
Mai Xuan Bach <maixuanbach174@gmail.com>
2023-06-29 10:33:27 +0700

    main

--- main.cpp
+++ main.cpp
@@ -1,38 +1,49 @@
 #include "Trie.h"
-#include "MyHashMap.cpp"
+#include "MyHashMap.h"
 #include <iostream>
+#include <io.h>
+#include <fcntl.h>
 using namespace std;
 
 int main()
 {
-    MyHashMap<string, string> m(3);
-    m.insert("an animal", "dog");
-    m.insert("a drink", "coke");
-    m.insert("a sport", "football");
-    cout << m.insert("no insert", "non_sert") << endl;
+    // MyHashMap<string, string> m(3);
+    // m.insert("an animal", "dog");
+    // m.insert("a drink", "coke");
+    // m.insert("a sport", "football");
+    // cout << m.insert("no insert", "non_sert") << endl;
 
-    cout << m.find("an animal")->val << endl;
-    cout << m.find("a drink")->val << endl;
-    cout << m.find("a sport")->val << endl;
-    cout << m.find("nos insert") << endl;
+    // cout << m.find("an animal")->val << endl;
+    // cout << m.find("a drink")->val << endl;
+    // cout << m.find("a sport")->val << endl;
+    // cout << m.find("nos insert") << endl;
 
-    cout << "_________________________________" << endl;
+    // cout << "_________________________________" << endl;
 
-    cout << m.erase("an animal")->val << endl;
-    cout << m.erase("an aninmal") << endl;
-    cout << m.find("an animal") << endl;
-    cout << m.find("a sport")->val << endl;
-    cout << m.find("a drink")->val << endl;
+    // cout << m.erase("an animal")->val << endl;
+    // cout << m.erase("an aninmal") << endl;
+    // cout << m.find("an animal") << endl;
+    // cout << m.find("a sport")->val << endl;
+    // cout << m.find("a drink")->val << endl;
 
-    cout << "__________________________________" << endl;
+    // cout << "__________________________________" << endl;
 
-    m.erase("a drink");
-    cout << m.find("a sport")->val << endl;
-    cout << m.find("a drink") << endl;
+    // m.erase("a drink");
+    // cout << m.find("a sport")->val << endl;
+    // cout << m.find("a drink") << endl;
 
-    cout << "__________________________________" << endl;
-    cout << m.size() << endl;
-    cout << m.empty() << endl;
+    // cout << "__________________________________" << endl;
+    // cout << m.size() << endl;
+    // cout << m.empty() << endl;
+
+    _setmode(_fileno(stdin), _O_U8TEXT);
+    _setmode(_fileno(stdout), _O_U8TEXT);
+
+    wchar_t c = L'ă';
+    wcout <<  c << endl;
+
+
+
 
     return 0;
 }
