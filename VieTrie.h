d140491a35ae4978b18b15fd2bfc09e3c880c3a4
TrangDangDucTin <115961028+trangdangductin@users.noreply.github.com>
2023-07-19 01:43:19 +0700

    Update VieTrie.h

--- VieTrie.h
+++ VieTrie.h
@@ -17,5 +17,7 @@ struct VieTrieNode
 bool VieInsert(VieTrieNode *& root, wstring vieword, string def);
 VieTrieNode * VieFind(VieTrieNode *& root, wstring vieword);
 void VieDeallocate(VieTrieNode * root);
-
+VieTrieNode* removefunction(VieTrieNode* root, string word, int depth);
+bool isEmpty(VieTrieNode* root);
+VieTrieNode* remove(VieTrieNode* root, string word);
 #endif
