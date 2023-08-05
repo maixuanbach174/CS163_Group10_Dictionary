ed6b22c24bf8cb9a7fe4b7a3cd1dbe4f08598441
Vinhapcs <128242214+Vinhapcs@users.noreply.github.com>
2023-08-05 08:34:01 +0700

    update random definition

--- getRandomDefintion.cpp
+++ getRandomDefintion.cpp
@@ -1,10 +1,11 @@
 #include "Trie.h"
 #include <bits/stdc++.h>
+
 using namespace std;
 wstring getRandomDefintion(TrieNode* root)
 {
     TrieNode *cur = root;
-
+    wstring fulldef = L"";
     srand(time(0));
 
     while ((cur != nullptr) && (!cur->isEndOfWord))
@@ -16,7 +17,29 @@ wstring getRandomDefintion(TrieNode* root)
         }
         while (!cur->children[index]);
         cur=cur->children[index];
-        if ((cur)&&(cur->isEndOfWord)) return cur->definition;
+        if ((cur)&&(cur->isEndOfWord)) fulldef=cur->definition;
+    }
+    wstringstream line(fulldef);
+    wstring line;
+    vector <wstring> def;
+    vector <wstring> normal;
+    while (getline(line,fulldef))
+    {
+        if (line[0]==L'-') def.push_back(line);
+        normal.push_back(line);
+    }
+    if (!def.empty())
+    {
+        srand(0);
+        int index=rand%def.size();
+        return def[index];
+    }
+    else
+    {
+        srand(0);
+        int index=rand%normal.size();
+        return normal[index];
     }
 
 }
+
