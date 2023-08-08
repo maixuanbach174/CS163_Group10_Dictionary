aaf54bce788d94e0724325de7050a4ce7133801a
Vinhapcs <128242214+Vinhapcs@users.noreply.github.com>
2023-08-08 14:34:50 +0700

    split definition

--- splitDefinition.cpp
+++ splitDefinition.cpp
@@ -1,5 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
+//vector <wstring> splitDefinition(wstring definition)
+//{
+//    vector <wstring> res;
+//    wstringstream ss(definition);
+//    wstring line;
+//    while (getline(ss,line))
+//    {
+//        if ((line[0]==L'-')or(iswalpha(line[0]))or(line[0]==L'+'))
+//        res.push_back(line);
+//        //wcout<<line<<endl;
+//    }
+//    return res;
+//}
+//vector <wstring> splitExample(wstring definition)
+//{
+//    vector <wstring> res;
+//    wstringstream ss(definition);
+//    wstring line;
+//    while (getline(ss,line))
+//    {
+//        if (line[0]==L'=') res.push_back(line);
+//    }
+//    return res;
+//}
 vector <wstring> splitDefinition(wstring definition)
 {
     vector <wstring> res;
@@ -7,33 +31,20 @@ vector <wstring> splitDefinition(wstring definition)
     wstring line;
     while (getline(ss,line))
     {
-        if ((line[0]==L'-')or(iswalpha(line[0]))or(line[0]==L'+'))
-        res.push_back(line);
-        //wcout<<line<<endl;
+        if ((line[0]==L'=')or(line[0]==L'-')or(iswalpha(line[0]))or(line[0]==L'+')) res.push_back(line);
     }
     return res;
 }
-vector <wstring> splitExample(wstring definition)
+int main()
 {
-    vector <wstring> res;
-    wstringstream ss(definition);
-    wstring line;
-    while (getline(ss,line))
+    wstring def=L"";
+    def += L"lam banh" + wstring(L"\n"); // Use double quotes for the newline character
+    def += L"da banh";    //def+=L"da banh";
+    vector <wstring> afterSPlit=splitDefinition(def);
+    for (int i=0;i<afterSPlit.size();i++)
     {
-        if (line[0]==L'=') res.push_back(line);
+        wcout<<afterSPlit[i]<<endl;
     }
-    return res;
+//wcout<<def;
+    return 0;
 }
-//int main()
-//{
-//    wstring def=L"";
-//    def += L"lam banh" + wstring(L"\n"); // Use double quotes for the newline character
-//    def += L"da banh";    //def+=L"da banh";
-//    vector <wstring> afterSPlit=splitDefinition(def);
-//    for (int i=0;i<afterSPlit.size();i++)
-//    {
-//        wcout<<afterSPlit[i]<<endl;
-//    }
-////wcout<<def;
-//    return 0;
-//}
