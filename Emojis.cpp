1ec85214d1290c8b090208d12a78ead1098ebb8e
meomeodicode <128510157+meomeodicode@users.noreply.github.com>
2023-08-12 10:38:39 +0700

    Emojis.cpp

--- Emojis.cpp
+++ Emojis.cpp
@@ -1,28 +1,5 @@
 #include <iostream>
-#include <fstream>
 #include <windows.h>
-#include <string>
-#include <sstream>
-#include <io.h>
-#include <codecvt>
-#include <fcntl.h>
-#include "VieTrie.h";
-using namespace std;
-
-
-void readEmoji(VieTrieNode* root)
-{
-	ifstream in("emoji_df.csv");
-	SetConsoleOutputCP(CP_UTF8);
-	if (!in)
-		cout << "Can't open";
-	string line, emo, limiter = ",", def;
-	while (getline(in, line))
-	{
-		size_t pos = line.find(limiter);
-		emo = line.substr(0, pos);
-		def = line.substr(pos + 1);
-		#include <windows.h>
 #include <string>
 #include <sstream>
 #include <io.h>
@@ -60,10 +37,6 @@ int main()
 	return 0;
 }
 
-	}
-	in.close();
-	out.close();
-}
 
 //int main()
 //{
