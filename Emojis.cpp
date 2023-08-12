25b2f016e17185cab272f5bf8fc9981ce6058105
meomeodicode <128510157+meomeodicode@users.noreply.github.com>
2023-08-12 12:02:16 +0700

    Emojis.cpp

--- Emojis.cpp
+++ Emojis.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-#include <fsteam>
+#include <fstream>
 #include <string>
 #include <io.h>
 #include <codecvt>
 #include <fcntl.h>
+#include <vector>
 #include "VieTrie.h"
 using namespace std;
 
 
-void readEmoji(VieTrieNode* root)
+void readEmoji(VieTrieNode* root, vector<wstring>& emo, vector<wstring>& def)
 {
-	wifstream in("emoji_df.csv");
+	wifstream in("emoticons.txt");
 	_setmode(_fileno(stdin), _O_U8TEXT);
 	_setmode(_fileno(stdout), _O_U8TEXT);
 	locale loc(locale(), new codecvt_utf8_utf16<wchar_t>);
@@ -18,13 +19,14 @@ void readEmoji(VieTrieNode* root)
 	//out.imbue(loc);
 	if (!in)
 		cout << "Can't open";
-	wstring line, emo, limiter = L",", def;
+	wstring line, emoSub, limiter = L"  ", defSub;
 	while (getline(in, line))
 	{
 		size_t pos = line.find(limiter);
-		emo = line.substr(0, pos);
-		def = line.substr(pos + 1);
-		VieInsert(root, emo, def);
+		emoSub = line.substr(0, pos);
+		defSub = line.substr(pos + 1);
+		emo.push_back(emoSub);
+		def.push_back(defSub);
 		//wcout << line;
 	}
 	in.close();
